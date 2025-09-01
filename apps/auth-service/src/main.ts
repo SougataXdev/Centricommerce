a20d4ec28f18df40bb3d43ffff1bfd19afebc7b0
SougataXdev <iaamsougata@gmail.com>
2025-09-01 11:41:05 +0530

    feat: add dotenv dependency, integrate cookie-parser, and update main.ts for error handling

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from "cors"
+import { errorMiddleware } from '../../../libs/middlewares/errorMiddleware';
+import cookieParser from 'cookie-parser';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
@@ -11,6 +13,12 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+
+
+app.use(errorMiddleware);
+
+app.use(express.json({ limit: '100mb' }));
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
